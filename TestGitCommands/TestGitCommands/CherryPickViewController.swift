578205acf1d3638080be33c0e401b0173f40b4cb
Marco di Fresco <marco.difresco@metide.com>
2022-04-05 13:10:44 +0200

    start cherry pick

---
+++ TestGitCommands/TestGitCommands/CherryPickViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CherryPickViewController.swift
+//  TestGitCommands
+//
+//  Created by Marco di Fresco on 05/04/22.
+//
+
+import UIKit
+
+class CherryPickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
--- TestGitCommands/TestGitCommands/RedViewController.swift
+++ TestGitCommands/TestGitCommands/RedViewController.swift
@@ -18,6 +18,14 @@ class RedViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func touchUpInsideCherryPick(_ sender: Any) {
+        self.tableListQrCodeReader = LeDeviceListView(nibName: "LeDeviceListView", bundle: nil)
+        self.tableListQrCodeReader?.modalTransitionStyle = .coverVertical
+        self.tableListQrCodeReader?.modalPresentationStyle = .fullScreen
+        self.present(self.tableListQrCodeReader!, animated: true)
+    }
+    
     @IBAction func touchUpInsideDismissButton(_ sender: Any) {
         self.dismiss(animated: true)
     }
